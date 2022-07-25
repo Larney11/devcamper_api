12280391a4397bb4f89b07d7118505c60098c286
Larney11 <20336495+Larney11@users.noreply.github.com>
2022-07-25 12:22:04 +0100

    MongoDB, Bootcamp controller, model, error handling

--- server.js
+++ server.js
@@ -1,14 +1,22 @@
 const express = require('express');
-require('dotenv').config({ path: './config/config.env' });
-// const logger = require('./middleware/logger');
 const morgan = require('morgan');
+const colors = require('colors');
+const errorHandler = require('./middleware/error');
+const connectDB = require('./config/db');
+
+// Load env vars
+require('dotenv').config({ path: './config/config.env' });
+
+// Connect to database
+connectDB();
 
 // Route files
 const bootcamps = require('./routes/bootcamps');
 
 const app = express();
 
-// app.use(logger);
+// Body parser
+app.use(express.json());
 
 //  Dev logging middleware
 if (process.env.NODE_ENV === 'development') {
@@ -18,9 +26,20 @@ if (process.env.NODE_ENV === 'development') {
 // Mount routers
 app.use('/api/v1/bootcamps', bootcamps);
 
+app.use(errorHandler);
+
 const PORT = process.env.PORT || 7777;
 
-app.listen(
+const server = app.listen(
   PORT,
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
 );
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`.red);
+  // Close server & exit process
+  server.close(() => process.exit(1))
+});
